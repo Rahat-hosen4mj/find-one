32f6a81dcf8780b20935633a3002a063fcac29ef
Your Name <you@example.com>
2022-05-21 15:31:07 +0600

    question er answer part hoeyce

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({cart}) => {
+    console.log(cart);
+    let total = 0;
+    let name = '';
+    for(const laptop of cart){
+        total = total + laptop.price;
+        name = name + laptop.brand + ' ';
+    }
     return (
         <div className='cart'>
             <h2>Find One</h2>
-            <p>Name : {cart.length} </p>
+            <p>Name : {name} </p>
+            <p>Price : {total} </p>
         </div>
     );
 };
